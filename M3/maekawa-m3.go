fe7dd308bd0a1454c3ea99c234ab1fd2551a1e63
Logkesh <logkeshhemanth@gmail.com>
2023-06-06 22:23:40 +0530

    still needs quorum validation

--- M3/maekawa-m3.go
+++ M3/maekawa-m3.go
@@ -21,39 +21,65 @@ var colorWhite = "\033[37m"
 type Process struct {
 	id    int
 	state string
+	quorum []int
+	inreq chan int
+	inrel chan int
+	response chan int
 	queue []int
 	data  *int
 }
 
 var processes []*Process
+var coterie [][]int
 
 // NewProcess creates a new process.
 func NewProcess(id int, data *int) *Process {
 	return &Process{
 		id:    id,
 		state: "alive",
-		queue: make([]int, 0),
+		quorum: []int{},
+		inreq: make(chan int,len(processes)),
+		inrel: make(chan int,len(processes)) ,
+		response: make(chan int,len(processes)),
+		queue: []int{},
 		data:  data,
+	}
+}
+
+func (process *Process) ManageRequest() {
+	for {
+		select {
+			case pid := <- process.inreq:
+				if len(process.queue) == 0 {
+					process.queue = append(process.queue, pid)
+					processes[pid].response <- process.id
+				} else {
+					process.queue = append(process.queue, pid)
+				}
+			case <- process.inrel:
+				process.queue = process.queue[1:]
+				if len(process.queue) > 0 { processes[process.queue[0]].response <- process.id}
+		}
 	}
 }
 
 // RequestCS requests permission to enter the critical section.
 func (process *Process) RequestCS() {
 
-	process.state = "ready"
-
 	fmt.Println(string(colorCyan), "Process", process.id, "requests CS", string(colorReset))
-
-	// Send request messages to all processes in the quorum.
-	for _, pid := range process.queue {
+	
+	for _, pid := range process.quorum {
 		fmt.Println("Process", process.id, "sends request message to", pid)
+		processes[pid].inreq <- process.id
 	}
 
 	// Wait for reply messages from a majority of processes in the quorum.
+	fmt.Println("Process", process.id, "waits for reply message from all processes")
+
 	granted := false
-	for i := 0; i < len(process.queue); i++ {
+	for i := 0; i < len(process.quorum); i++ {
 		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-		pid := process.queue[i]
+		pid := <- process.response
 		fmt.Println("Process", process.id, "waits for reply message from", pid)
 
 		// If a majority of processes have granted permission, enter the critical section.
@@ -80,23 +106,25 @@ func (process *Process) ExecuteCS() {
 	*process.data = process.id * process.id
 
 	fmt.Println(string(colorGreen), *process.data, string(colorReset))
+	
+	time.Sleep(time.Duration(1500) * time.Millisecond)
 
 }
 
 // ReleaseCS releases the critical section.
 func (process *Process) ReleaseCS() {
 	fmt.Println(string(colorRed), "Process", process.id, "releases CS", string(colorReset))
-
 	// Send release messages to all processes in the quorum.
-	for _, pid := range process.queue {
+	for _, pid := range process.quorum {
 		fmt.Println("Process", process.id, "sends release message to", pid)
+		processes[pid].inrel <- process.id
 	}
-
-	// Change state to ready.
+	// Change state to alive.
 	process.state = "alive"
 }
 
 func (process *Process) StartProcess(wg *sync.WaitGroup) {
+	process.state = "ready"
 	fmt.Println(string(colorYellow), "Process", process.id, "wants to get into CS", string(colorReset))
 
 	// requests permission to enter the critical section.
@@ -104,6 +132,8 @@ func (process *Process) StartProcess(wg *sync.WaitGroup) {
 
 	if process.state == "critical" {
 		process.ExecuteCS()
+	} else {
+		fmt.Println(string(colorRed), "Process", process.id, " cannot get into CS", string(colorReset))
 	}
 
 	process.ReleaseCS()
@@ -121,26 +151,35 @@ func main() {
 	fmt.Printf("\n")
 
 	// Create a shared Variable.
-	var shared_value int
+	var shared_value int = -1
 
 	// Create a set of processes.
 	var numProcs int
 	fmt.Println(string(colorCyan), "Enter the number of processes: ", string(colorReset))
-	fmt.Scanln(&numProcs)
+	// fmt.Scanln(&numProcs)
+	numProcs = 5
 
 	processes = make([]*Process, numProcs)
+	coterie = [][]int{
+		{2,3},
+		{2,4},
+		{1,4},
+		{1,2,4},
+		{1,2,3},
+	}
 	for i := 0; i < numProcs; i++ {
 		processes[i] = NewProcess(i, &shared_value)
+		go processes[i].ManageRequest()
 	}
-
+	
 	// Set the quorums for each process.
 	for i := 0; i < numProcs; i++ {
-		processes[i].queue = []int{(i + 1) % numProcs, (i + 2) % numProcs}
+		processes[i].quorum = coterie[i]
 	}
 
-	var numberOfIterations int
+	var numberOfCSaccess int = 5
 	fmt.Println(string(colorCyan), "Enter the number of iterations: ", string(colorReset))
-	fmt.Scanln(&numberOfIterations)
+	// fmt.Scanln(&numberOfCSaccess)
 
 	// Print the quorums.
 	fmt.Printf("\n")
@@ -149,7 +188,7 @@ func main() {
 	fmt.Println(string(colorPurple), "-------------------", string(colorReset))
 	for _, process := range processes {
 		fmt.Printf("Process %d: ", process.id)
-		for _, pid := range process.queue {
+		for _, pid := range process.quorum {
 			fmt.Printf("%d ", pid)
 		}
 		fmt.Printf("\n")
@@ -159,14 +198,15 @@ func main() {
 	fmt.Printf("\n")
 
 	var wg sync.WaitGroup
-	wg.Add(numberOfIterations)
+	wg.Add(numberOfCSaccess)
+
 	// Create Multiple simulations
-	for i := 0; i < numberOfIterations; i++ {
-		var aliveProcesses []Process
+	for i := 0; i < numberOfCSaccess; i++ {
+		var aliveProcesses = []*Process{}
 
 		for j := 0; j < len(processes); j++ {
 			if processes[j].state == "alive" {
-				aliveProcesses = append(aliveProcesses, *processes[j])
+				aliveProcesses = append(aliveProcesses, processes[j])
 			}
 		}
 
@@ -180,3 +220,4 @@ func main() {
 	}
 	wg.Wait()
 }
+
